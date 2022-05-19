2a7678071c4f4bea8c36d864467731797280da81
Alejandro Lima <73153439+Drojann@users.noreply.github.com>
2022-05-19 00:08:38 -0500

    eslint

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,29 +1,27 @@
-const Reader = require ('../../lib/utils/Reader')
-const ExplorerService = require ('../../lib/services/ExplorerService')
+const Reader = require ("../../lib/utils/Reader");
+const ExplorerService = require ("../../lib/services/ExplorerService");
 
-describe("testing explorer service", () => {
+describe("testing explorer service", () => {const explorers = Reader.readJsonFile("explorers.json");
 
-    const explorers = Reader.readJsonFile('explorers.json')
+    test("filter by mission", () => {
 
-    test('filter by mission', () => {
-
-      const missionNode = ExplorerService.filterByMission(explorers, "node");
-      expect(missionNode[0]).toMatchObject({mission: "node"})
-      console.log(missionNode)
+        const missionNode = ExplorerService.filterByMission(explorers, "node");
+        expect(missionNode[0]).toMatchObject({mission: "node"});
+        console.log(missionNode);
     });
 
-    test('get explorers by mission', () => {
+    test("get explorers by mission", () => {
 
         const explorersInMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-        expect(explorersInMission).toBe(10)
-        console.log(explorersInMission)
+        expect(explorersInMission).toBe(10);
+        console.log(explorersInMission);
     });
 
-    test('get usernames by mission', () => {
+    test("get usernames by mission", () => {
 
         const usernameInMission = ExplorerService.getExplorersUserNameByMission(explorers, "node");
-        expect(usernameInMission).toContain('ajolonauta15')
-        console.log(usernameInMission)
+        expect(usernameInMission).toContain("ajolonauta15");
+        console.log(usernameInMission);
     });  
-})
+});
 
